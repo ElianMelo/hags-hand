3f464b63c04647f432a2ebc7fd71a4e18cbf71d6
Elian Melo <elianmelomorais@gmail.com>
2025-04-05 09:54:15 -0300

    Fix enemy target

--- Assets/_Project/Scripts/Enemy/EnemyTarget.cs
+++ Assets/_Project/Scripts/Enemy/EnemyTarget.cs
@@ -4,7 +4,7 @@ public class EnemyTarget : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Enemy"))
+        if(other.CompareTag("Enemy"))
         {
             Enemy enemy = other.gameObject.GetComponent<Enemy>();
             InterfaceSystemManager.Instance.WaveDamage(enemy.GetDamage());
